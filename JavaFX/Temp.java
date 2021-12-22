36eae729d08a70d611aae16ef1de7abc4b918584
Fernando Contreras <luisfernando_contreras@hotmail.com>
2021-12-21 22:02:40 -0600

    Test.java files deleted

--- JavaFX/Temp.java
+++
@@ -1,8 +0,0 @@
-public class Temp {
-    public static void main(String[] args) {
-        int i = 1;
-        boolean condition = false;
-        i += condition ? 1 : 2;
-        System.out.println(i);
-    }
-}
--- deitel/temporales/modulo.java
+++
@@ -1,8 +0,0 @@
-public class modulo{
-  public static void main(String[] args){
-    for (int count = 0; count < 100; count++){
-      if(100 % 50 == 0)
-        System.out.println("si");
-    }
-  }
-}
